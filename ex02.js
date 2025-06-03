f8872759f8b52025152de14a347c97d10a741ade
Marina <marishakis88@gmail.com>
2025-06-03 16:57:28 +0500

    ex02: переменные и вывод

---
+++ ex02.js
@@ -0,0 +1,6 @@
+let name = "Марина";
+const age = 41;
+
+console.log("Привет, меня зовут " + name + "!");
+console.log("Мне " + age + " — и это только начало!");
+alert("Добро пожаловать, " + name + "!");
