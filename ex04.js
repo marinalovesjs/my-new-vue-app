f84ad9a7fd06fa84b3c459a7bbd05177922e368f
Marina <marishakis88@gmail.com>
2025-06-03 18:18:19 +0500

    ex04: калькулятор суммы

---
+++ ex04.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Введите первое число:");
+let num2 = prompt("Введите второе число:");
+
+let result = Number(num1) + Number(num2);
+
+alert("Сумма: " + result);
+console.log("Пользователь сложил " + num1 + " + " + num2 + " = " + result);
